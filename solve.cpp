bb80a245232910a99a1b01318eb59f4ab66cbeeb
tiimuur <98406636+tiimuur@users.noreply.github.com>
2024-02-18 21:14:15 +0300

    Create solve.cpp

---
+++ solve.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <set>
+
+#define ll long long
+#define all(v) ((v).begin(), (v).end())
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 1;
+
+signed main() {
+    // freopen("test.in", "r", stdin);
+    // freopen("test.out", "w", stdout);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+    // int t; std::cin >> t;
+    // while(t--) {
+        
+    // }
+    return 0;
+}
